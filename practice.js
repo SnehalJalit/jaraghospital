f3202ee2b185d6b84767ca2485200eaa1b25e81d
SnehalJalit <139626953+SnehalJalit@users.noreply.github.com>
2024-10-19 13:29:35 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,56 @@
+// function getname(city1,city2) {
+//     console.log(this.fname+" "+this.lname + "live in " + city1,city2);
+    
+// }
+// let person={
+//     fname:"John",
+//     lname:"Doe",
+
+// }
+// // let cities=['pune','nagpur']
+// // getname.apply(person,cities);
+// let bindname=getname.bind(person);
+// bindname('delhi','pune');
+// let a1=[1,2,3];
+// a1.indexOf(1);
+// console.log(a1.indexOf(3));
+// console.log(a1.includes(7));
+// console.log(a1.unshift(6));
+// console.log(a1);
+
+
+// var date=new Date();  
+// var day=date.getDate();  
+// var month=date.getMonth()+1;  
+// var year=date.getFullYear();  
+// console.log
+// ("<br>Date is: "+day+"/"+month+"/"+year);  
+
+// var today=new Date();  
+// var h=today.getHours();  
+// var m=today.getMinutes();  
+// var s=today.getSeconds();  
+// // document.getElementById('txt').innerHTML=h+":"+m+":"+s;  
+// console.log( "time :" +h+"/"+m+"/"+s);
+// const staffsDetails = [
+//     { name: "Jam Josh", age: 44, salary: 4000, currency: "USD" },
+//     { name: "Justina Kap", age: 34, salary: 3000, currency: "USD" },
+//     { name: "Chris Colt", age: 37, salary: 3700, currency: "USD" },
+//     { name: "Jane Doe", age: 24, salary: 4200, currency: "USD" }
+//   ];
+//   staffsDetails.forEach((staffDetail) => {
+//     let sentence = `I am ${staffDetail.name} a staff of Royal Suites.`;
+//     console.log(sentence);
+//   });
+//   const scores = [12, 55, 70, 47];
+
+// let total = 0;
+// scores.forEach((score) => {
+//   total += score;
+// });
+
+let fruits=['apple','banana','custurdapple','grapes'];
+fruits.forEach((listfruit)=>{
+console.log(listfruit);
+
+})
